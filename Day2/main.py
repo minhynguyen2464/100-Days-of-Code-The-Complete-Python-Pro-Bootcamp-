582552c9d35bc442d12f6d701b4db8148e0db55c
HP Victus <minhynguyen97@gmail.com>
2023-10-23 00:23:38 +0700

    Complete Day 2

---
+++ Day2/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator")
+total_bill = float(input("What was the total bill? $"))
+percent_tip = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
+tip_bill = total_bill+total_bill*(percent_tip/100)
+spilt_bill = round(tip_bill/people,2)
+print(f"Each person should pay: {spilt_bill}")
---
+++ main.py
@@ -0,0 +1,7 @@
+age = int(input("What is your current age? "))
+live_year = 90
+year_left = 90 - age
+days_left = year_left*365
+weeks_left = year_left*52
+month_left = year_left*12
+print(f"You have {days_left} days, {weeks_left} weeks, and {month_left} months left.")
